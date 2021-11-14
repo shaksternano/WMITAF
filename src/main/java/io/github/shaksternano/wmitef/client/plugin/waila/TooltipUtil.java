c1b442b1b7a2b2b2630539ffbe1d894028ae356a
Shukai Zhang <shukai.zhang@hotmail.com>
2021-11-14 22:54:55 +0000

    Update TooltipUtil.java

--- src/main/java/io/github/shaksternano/wmitef/client/plugin/waila/TooltipUtil.java
+++ src/main/java/io/github/shaksternano/wmitef/client/plugin/waila/TooltipUtil.java
@@ -11,21 +11,24 @@ import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Map;
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.Set;
 
 @Environment(EnvType.CLIENT)
 public final class TooltipUtil {
 
     private TooltipUtil() {}
 
-    // Returns the name of the mod that adds the first registered enchantment on an enchanted book.
+    // Returns the name of the mod that adds the first enchantment on an enchanted book.
     @Nullable
     public static String replaceEnchantmentModName(ItemStack stack) {
         if (stack.isOf(Items.ENCHANTED_BOOK)) {
-            Map<Enchantment, Integer> enchantments = EnchantmentHelper.get(stack);
+            Set<Enchantment> enchantments = EnchantmentHelper.get(stack).keySet();
+            Iterator<Enchantment> enchantmentsIterator = enchantments.iterator();
 
-            for (Enchantment enchantment : enchantments.keySet()) {
+            if (enchantmentsIterator.hasNext()) {
+                Enchantment enchantment = enchantmentsIterator.next();
                 Identifier enchantmentId = EnchantmentHelper.getEnchantmentId(enchantment);
 
                 if (enchantmentId != null) {
