b45ce31a742aa6a0866d7c7f16b889bd73fd4d8c
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2022-08-11 10:51:45 +0100

    Update Wmitaf.java

--- common/src/main/java/io/github/shaksternano/wmitaf/client/Wmitaf.java
+++ common/src/main/java/io/github/shaksternano/wmitaf/client/Wmitaf.java
@@ -1,9 +1,5 @@
 package io.github.shaksternano.wmitaf.client;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
-@Environment(EnvType.CLIENT)
 public class Wmitaf {
 
     public static final String MOD_ID = "wmitaf";
